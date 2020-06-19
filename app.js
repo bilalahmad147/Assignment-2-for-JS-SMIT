ceafaa952a0097dc5f586fa96486c4a54081892f
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-06-19 11:53:47 -0800

    Update app.js

---
+++ app.js
@@ -0,0 +1,71 @@
+// String Method
+
+// var fName = prompt("enter your first name :");
+// var lName = prompt("enter your last name :");
+// var fullName = fName + " " + lName;
+// document.write("Welcome " + fullName);
+
+
+// var favPhone = prompt("Enter favourit Phone :");
+// var lenOfInput = favPhone.length;
+// document.write("My fav phone is : " + favPhone + '<br />');
+// document.write("Length of Input = "+ lenOfInput);
+
+
+// var given = prompt("Enter string :" , "pakistani");
+// var findIndex = given.indexOf('n');
+// document.write("Given string is : " + given + '<br />');
+// document.write("index number : " + findIndex);
+
+
+// var given = prompt("Enter string :" , "hello world");
+// var findIndex = given.lastIndexOf('l');
+// document.write("Given string is : " + given + '<br />');
+// document.write("index number : " + findIndex);
+
+
+// var given = prompt("Enter string :" , "pakistani");
+// var findIndex = given.charAt(3);
+// document.write("Given string is : " + given + '<br />');
+// document.write("letter at given index : " + findIndex);
+
+
+// var str1 = "Bilal ";
+// var str2 = "Ahmad!";
+// var res = str1.concat(str2);
+// document.write(res);
+
+
+// var cityName = 'hydrabad';
+// var replaceName = cityName.replace('hydr', 'islam');
+// document.write(replaceName);
+
+
+// var message = 'Ali and Sami are best friends. They play cricket and football together.';
+// var replaceMessage = message.replace(/and/g, '&');
+// document.write(message + '<br />');
+// document.write(replaceMessage);
+
+
+// var num = 472;
+// document.write('value : ' + num + '<br />');
+// var typeNum = typeof(num);
+// document.write('type : ' + typeNum + '<br />');
+// var typeNum = typeof(num.toString());
+// document.write('value : ' + num + '<br />');
+// document.write('type : ' + typeNum);
+
+
+// var userInput = prompt("Enter string : ");
+// document.write(userInput + '<br />');
+// var userInputToUpper =  userInput.toUpperCase();
+// document.write(userInputToUpper);
+
+
+// var userInput = prompt("Enter string : ");
+// document.write(userInput + '<br />');
+// var firstChar =  (userInput.charAt(0)).toUpperCase();
+// var remChar =  (userInput.slice(1, )).toLowerCase();
+// document.write(firstChar + remChar);
+
+
